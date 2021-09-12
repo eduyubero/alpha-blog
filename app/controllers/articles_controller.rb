15f1ffb1227dc903b65c2d931b5e9f934b1539a3
Edu Yubero <edu.yubero@gmail.com>
2021-09-12 19:38:04 +0200

    All REST actions completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,41 @@ class ArticlesController < ApplicationController
         @articles = Article.all        
     end
 
+    def new        
+        @article = Article.new()
+    end
+
+    def edit        
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :author, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            #redirect_to article_path(@article) # Also the following shorter version:
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :author, :description))
+            flash[:notice] = "Article was updated successfully."
+            #redirect_to article_path(@article) # Also the following shorter version:
+            redirect_to @article
+        else
+            render 'edit'
+        end        
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
 end
